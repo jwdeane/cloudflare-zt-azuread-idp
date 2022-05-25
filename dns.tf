fcb996b9c52f31172ccdb58919b99f390b86aac4
James Deane <jdeane@cloudflare.com>
2022-05-26 00:09:11 +1000

    rename zone resource

--- dns.tf
+++ dns.tf
@@ -1,10 +1,10 @@
 # Subdomain for testing Azure AD
-resource "cloudflare_zone" "azure_cflr_win" {
-  zone = "${var.cloudflare_domain}"
+resource "cloudflare_zone" "default" {
+  zone = var.cloudflare_domain
 }
 
 resource "cloudflare_record" "azure_custom_domain" {
-  zone_id = cloudflare_zone.azure_cflr_win.id
+  zone_id = cloudflare_zone.default.id
   type = "TXT"
   name = "@"
   value = var.azure_custom_domain_txt
